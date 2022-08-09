309742d9852c8b6a75877f9c124b26ef066c7505
Doomsdhg <tohanknv@yandex.ru>
2022-08-09 17:43:41 +0300

    initial commit

---
+++ src/components/MainPage/MainPage.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './MainPage.scss';
+
+interface MainPageProps {}
+
+const MainPage: FC<MainPageProps> = () => (
+  <div className="MainPage">
+    MainPage Component
+  </div>
+);
+
+export default MainPage;

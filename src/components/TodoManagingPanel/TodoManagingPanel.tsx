5e1b1d36d90116d4680431a935674ce066ec60c6
Doomsdhg <tohanknv@yandex.ru>
2022-08-10 17:47:30 +0300

    TODO-2: todo managing panel implemented, functionality of editing name and status of todo implemented

--- src/components/TodoManagingPanel/TodoManagingPanel.tsx
+++ src/components/TodoManagingPanel/TodoManagingPanel.tsx
@@ -1,14 +1,44 @@
-import React, { FC } from 'react';
+import React, { FC, useContext, useState } from 'react';
+import { SelectedTodoContext } from '../../context/SelectedTodoContext';
+import { useLocalStorage } from '../../hooks/localStorageHooks';
+import { Todo } from '../TodoList/TodoList';
 import './TodoManagingPanel.scss';
 
 interface TodoManagingPanelProps {}
 
-const TodoManagingPanel: FC<TodoManagingPanelProps> = () => (
-  <div className="todo-managing-panel-container">
-    <span>
-      TodoManagingPanel Component
-    </span>
-  </div>
-);
+const TodoManagingPanel: FC<TodoManagingPanelProps> = () => {
+  const {selectedTodo, selectedTodoIndex} = useContext(SelectedTodoContext);
+  const [todosArray, setTodosArray] = useLocalStorage<Todo[]>('todos', []);
+  const [todoName, setTodoName] = useState<string>(selectedTodo.name);
+  const [todoStatus, setTodoStatus] = useState<string>(selectedTodo.status);
+  const noTodoSelected = selectedTodo.name === '' && selectedTodo.status === '';
+
+  const saveChanges = () => {
+    const newArray: Todo[] = [...todosArray];
+    newArray[selectedTodoIndex] = new Todo(todoName, todoStatus);
+    setTodosArray([...newArray]);
+  }
+
+  return (
+    noTodoSelected ?
+    <span>no todo selected</span> :
+    <div className="todo-managing-panel-container">
+      <div>
+        <form>
+          <input type="text" defaultValue={selectedTodo.name} onChange={(e) => setTodoName(e.target.value)}>
+          </input>
+        </form>
+        <form>
+          <select onChange={(e) => setTodoStatus(e.target.value)}>
+            <option selected={selectedTodo.status === "awaits"}>awaits</option>
+            <option selected={selectedTodo.status === "in process"}>in process</option>
+            <option selected={selectedTodo.status === "finished"}>finished</option>
+          </select>
+        </form>
+        <button onClick={() => saveChanges()}>save</button>
+      </div>
+    </div>
+  )
+};
 
 export default TodoManagingPanel;
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,13 +1,20 @@
-import TodoList from '../../components/TodoList/TodoList';
+import { useState } from 'react';
+import TodoList, { Todo } from '../../components/TodoList/TodoList';
 import TodoManagingPanel from '../../components/TodoManagingPanel/TodoManagingPanel';
+import { SelectedTodoContext } from '../../context/SelectedTodoContext';
 import './MainPage.scss';
 
 export function MainPage(){
+  const [selectedTodo, setSelectedTodo] = useState(new Todo('',''));
+  const [selectedTodoIndex, setSelectedTodoIndex] = useState<number>(0);
+
   return (
-    <div className="main-page-container">
-      <TodoList />
-      <TodoManagingPanel />
-    </div>
+    <SelectedTodoContext.Provider value={{selectedTodo, setSelectedTodo, selectedTodoIndex, setSelectedTodoIndex}}>
+      <div className="main-page-container">
+        <TodoList />
+        <TodoManagingPanel />
+      </div>
+    </SelectedTodoContext.Provider>
 )};
 
 export default MainPage;
